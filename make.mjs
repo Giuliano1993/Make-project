508b6b2cb1e47569d9c5d59ce795069d66204e0b
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-12-18 13:05:57 +0100

    [ add ] laravel [ add ] init git repo [ add ] open code

--- make.mjs
+++ make.mjs
@@ -4,6 +4,7 @@ const file = fs.readFileSync('./.env.yaml','utf8');
 const env = YAML.parse(file)
 //const projectsPath = "C:/Users/giuli/Desktop/Projects";
 const projectsPath = env.PROJECTSDIR;
+const composerPath = env.COMPOSERPATH;
 
 
 let name;
@@ -16,13 +17,15 @@ if(!argv.name) {
 let projectType
 
 if(!argv.type) {
-  projectType = await question('Project type [PHP|Node|Rust]: ');
+  projectType = await question('Project type [PHP|Node|Rust|Laravel]: ');
 }else{
   projectType = argv.type;
 }
 cd(projectsPath)
 await $`mkdir ${name}`;
 cd(name);
+await $`git init -q`;
+await $`touch .gitignore`;
 switch (projectType) {
     case "PHP":
         await $`touch index.php`;
@@ -32,9 +35,18 @@ switch (projectType) {
         await $`npm init -y`
         await $`touch index.js`;
         await $`echo "console.log('Hello, World!')" > index.js`;
+        await $`echo "node_modules" > .gitignore`
         break;
     case "Rust":
         await $`cargo.exe init`
+        break;
+    case "Laravel":
+        await $`php.exe ${composerPath} create-project laravel/laravel .`
+        await $`npm install`
+        await $`php.exe artisan key:generate`
+        await $`echo "node_modules \n\r" > .gitignore`
+        await $`echo "vendor" > .gitignore`
+        //await $`php artisan serve`
         break;
     default:
         console.log("Not a valid project type")
@@ -43,3 +55,7 @@ switch (projectType) {
 
 await $`touch README.md`;
 await $`echo "# ${name}" > README.md`;
+
+if(argv.code){
+  await $`code .`;
+}
